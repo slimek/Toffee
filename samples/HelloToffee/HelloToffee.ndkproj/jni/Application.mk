ce0b55aa239928b5c7a058fd1f0e527b88dc5cf6
slimek <slime573@gmail.com>
2013-11-05 09:13:24 +0800

    [NDK] Add project files.

--- samples/HelloToffee/HelloToffee.ndkproj/jni/Application.mk
+++ samples/HelloToffee/HelloToffee.ndkproj/jni/Application.mk
@@ -1,6 +1,7 @@
 APP_PLATFORM := android-17
 APP_ABI := armeabi-v7a
 APP_STL := gnustl_static
-APP_CPPFLAGS := -std=c++11 -frtti -DCOCOS2D_DEBUG=1
+APP_CFLAGS := -g -fexceptions -DCOCOS2D_DEBUG=1
+APP_CPPFLAGS := -std=c++11 -frtti -fexceptions -g -DCOCOS2D_DEBUG=1
 
-NDK_TOOLCHAIN_VERSION := clang3.3
+NDK_TOOLCHAIN_VERSION := 4.8
